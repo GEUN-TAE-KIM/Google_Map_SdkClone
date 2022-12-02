128aab20af3dc690e2a28ffe3a328c492960c384
rmsxo5678 <rmsxo5678@naver.com>
2022-12-02 16:27:06 +0900

    제어 및 제스처

--- app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MapsActivity.kt
+++ app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MapsActivity.kt
@@ -25,6 +25,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
+
         mapFragment.getMapAsync(this)
     }
 
@@ -43,6 +44,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // 구글지도 켜서 해당 위치 오른쪽 마우스 클릭하면 위도경도 다 나옴
         val kotoy = LatLng(34.99490705490703, 135.7851237570075)
         map.addMarker(MarkerOptions().position(kotoy).title("기요미즈데라"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(kotoy))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(kotoy, 10f))
+        map.uiSettings.apply {
+            // 화면에 줌할수있는 +/- 를 나타내는 것
+            isZoomControlsEnabled = true
+            // 손가락 터치로 줌을 할수있냐없냐 를 나타낸 것
+           // isZoomGesturesEnabled = false
+            // 정적으로 맵을 보여주는 것 (동작불가 정지 화면)
+           // isScrollGesturesEnabled = false
+
+            // 자신의 장소를 버튼으로 활성화 하는 것 (자신의 위치 활성화 해야함)
+            isMyLocationButtonEnabled = true
+        }
     }
 }
