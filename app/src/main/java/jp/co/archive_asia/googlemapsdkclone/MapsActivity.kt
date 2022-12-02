348a2f19106484476a12064e9ad860c75efc85a6
rmsxo5678 <rmsxo5678@naver.com>
2022-12-02 16:00:47 +0900

    좌표 정보 얻기

--- app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MapsActivity.kt
+++ app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MapsActivity.kt
@@ -40,9 +40,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // 구글지도 켜서 해당 위치 오른쪽 마우스 클릭하면 위도경도 다 나옴
+        val kotoy = LatLng(34.99490705490703, 135.7851237570075)
+        map.addMarker(MarkerOptions().position(kotoy).title("기요미즈데라"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(kotoy))
     }
 }
