85a721d2f06363b24ffdc774609cdf5e9ed3b488
rmsxo5678 <rmsxo5678@naver.com>
2022-12-02 17:19:24 +0900

    지도 유형 변경

--- app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MapsActivity.kt
+++ app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MapsActivity.kt
@@ -2,6 +2,13 @@ package jp.co.archive_asia.googlemapsdkclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -29,32 +36,54 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-
-        // 구글지도 켜서 해당 위치 오른쪽 마우스 클릭하면 위도경도 다 나옴
-        val kotoy = LatLng(34.99490705490703, 135.7851237570075)
-        map.addMarker(MarkerOptions().position(kotoy).title("기요미즈데라"))
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(kotoy, 10f))
-        map.uiSettings.apply {
-            // 화면에 줌할수있는 +/- 를 나타내는 것
-            isZoomControlsEnabled = true
-            // 손가락 터치로 줌을 할수있냐없냐 를 나타낸 것
-           // isZoomGesturesEnabled = false
-            // 정적으로 맵을 보여주는 것 (동작불가 정지 화면)
-           // isScrollGesturesEnabled = false
-
-            // 자신의 장소를 버튼으로 활성화 하는 것 (자신의 위치 활성화 해야함)
-            isMyLocationButtonEnabled = true
-        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.map_types_menu, menu)
+        return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.normal_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+            R.id.hybrid_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            }
+
+            R.id.satellite_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            }
+
+            R.id.terrain_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            }
+            R.id.none_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_NONE
+            }
+        }
+        return true
+    }
+
+
+override fun onMapReady(googleMap: GoogleMap) {
+    map = googleMap
+
+    // 구글지도 켜서 해당 위치 오른쪽 마우스 클릭하면 위도경도 다 나옴
+    val kotoy = LatLng(34.99490705490703, 135.7851237570075)
+    map.addMarker(MarkerOptions().position(kotoy).title("기요미즈데라"))
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(kotoy, 10f))
+    map.uiSettings.apply {
+        // 화면에 줌할수있는 +/- 를 나타내는 것
+        isZoomControlsEnabled = true
+        // 손가락 터치로 줌을 할수있냐없냐 를 나타낸 것
+        // isZoomGesturesEnabled = false
+        // 정적으로 맵을 보여주는 것 (동작불가 정지 화면)
+        // isScrollGesturesEnabled = false
+
+        // 자신의 장소를 버튼으로 활성화 하는 것 (자신의 위치 활성화 해야함)
+        isMyLocationButtonEnabled = true
+    }
+    // 말그래도 패딩하는거 이동시키는것
+    // map.setPadding(0,0,300,0)
+}
 }
