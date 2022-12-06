b86e14cabeac474190f0598185e92e9644c3b7ea
rmsxo5678 <rmsxo5678@naver.com>
2022-12-06 11:07:24 +0900

    네비 및 설정

---
+++ app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MainActivity.kt
@@ -0,0 +1,26 @@
+package jp.co.archive_asia.googlemapsdkclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
