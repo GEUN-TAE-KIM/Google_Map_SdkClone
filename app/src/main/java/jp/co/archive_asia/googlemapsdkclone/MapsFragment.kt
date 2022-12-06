165d89e69bc76116e86ddf2a717c8885b652dea8
rmsxo5678 <rmsxo5678@naver.com>
2022-12-06 16:36:22 +0900

    내 위치 레이어 활성화

--- app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MapsFragment.kt
+++ app/src/main/java/jp/co/archive_asia/googlemapsdkclone/MapsFragment.kt
@@ -1,11 +1,13 @@
 package jp.co.archive_asia.googlemapsdkclone
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,11 +17,17 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import jp.co.archive_asia.googlemapsdkclone.databinding.FragmentMapsBinding
 import jp.co.archive_asia.googlemapsdkclone.databinding.FragmentPermissionBinding
+import jp.co.archive_asia.googlemapsdkclone.util.ExtensionFunctions.hide
+import jp.co.archive_asia.googlemapsdkclone.util.ExtensionFunctions.show
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MapsFragment : Fragment(), OnMapReadyCallback {
+class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
 
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var map: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,9 +36,9 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         _binding = FragmentMapsBinding.inflate(inflater, container, false)
 
-        binding.startButton.setOnClickListener {  }
-        binding.stopButton.setOnClickListener {  }
-        binding.resetButton.setOnClickListener {  }
+        binding.startButton.setOnClickListener { }
+        binding.stopButton.setOnClickListener { }
+        binding.resetButton.setOnClickListener { }
 
         return binding.root
     }
@@ -42,12 +50,34 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     }
 
     // 맵이 준비 될때마다 트리거하는 함수
-    override fun onMapReady(p0: GoogleMap) {
-
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.isMyLocationEnabled = true
+        map.setOnMyLocationButtonClickListener(this)
+        map.uiSettings.apply {
+            // 이동, 줌 등 다 하지 못하게 한것
+            isZoomControlsEnabled = false
+            isZoomGesturesEnabled = false
+            isRotateGesturesEnabled = false
+            isTiltGesturesEnabled = false
+            isCompassEnabled = false
+            isScrollGesturesEnabled = false
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        binding.hintTextView.animate().alpha(0f).duration = 1500
+        lifecycleScope.launch {
+            delay(2500)
+            binding.hintTextView.hide()
+            binding.startButton.show()
+        }
+        return false
     }
 }
